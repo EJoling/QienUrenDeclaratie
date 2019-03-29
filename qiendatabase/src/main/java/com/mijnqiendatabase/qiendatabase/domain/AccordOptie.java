799d1a2f2b84456fba5d125f047b159522efc0e2
Jordi1080 <jordi.hesselz@hotmail.com>
2019-03-29 16:37:31 +0100

    laatste commit voor het weekend

---
+++ qiendatabase/src/main/java/com/mijnqiendatabase/qiendatabase/domain/AccordOptie.java
@@ -0,0 +1,9 @@
+package com.mijnqiendatabase.qiendatabase.domain;
+
+
+public enum AccordOptie{
+	NIETINGEVULD, // 0
+	TEACCORDEREN, // 1
+	GOEDGEKEURD, // 2
+	AFGEKEURD // 3
+}
--- qiendatabase/src/main/java/com/mijnqiendatabase/qiendatabase/domain/Tijdsformulier.java
+++ qiendatabase/src/main/java/com/mijnqiendatabase/qiendatabase/domain/Tijdsformulier.java
@@ -3,6 +3,7 @@ package com.mijnqiendatabase.qiendatabase.domain;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,9 +15,18 @@ public class Tijdsformulier extends Formulier{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	@OneToMany
+	@OneToMany (fetch = FetchType.EAGER)
 	private Set<Dag> dagen;
 	
+	private AccordOptie accordStatus;
+
+	
+	public AccordOptie getAccordStatus() {
+		return accordStatus;
+	}
+	public void setAccordStatus(AccordOptie accordStatus) {
+		this.accordStatus = accordStatus;
+	}
 	public Set<Dag> getDagen() {
 		return dagen;
 	}
