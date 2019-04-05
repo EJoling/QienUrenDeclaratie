f8b062318894971ce2f3b9d58dc5caebed36c32e
Tim Kroon <timkroon0703@gmail.com>
2019-04-05 15:27:28 +0200

    tim7

---
+++ qiendatabase/src/test/java/com/mijnqiendatabase/qiendatabase/domain/DeclaratieformulierTest.java
@@ -0,0 +1,21 @@
+package com.mijnqiendatabase.qiendatabase.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class DeclaratieformulierTest {
+	
+	private Declaratieformulier declaratieformulier = new Declaratieformulier();
+	
+	@Test
+	public void testGetAndSetFortest() {
+		
+		this.declaratieformulier.setTest("NietDus");
+		
+		Assert.assertEquals("WelDus", this.declaratieformulier.getTest());
+		
+	}
+
+}
