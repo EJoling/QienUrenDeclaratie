fe78e4200827c835b6580c0e4bca7b4a7f221598
Raymond Loman <raymond@carpago.nl>
2019-04-10 17:31:19 +0200

    Fix two broken unittests(KostenTest and DeclaratieFormulierTest

--- qiendatabase/src/test/java/com/mijnqiendatabase/qiendatabase/domain/DeclaratieformulierTest.java
+++ qiendatabase/src/test/java/com/mijnqiendatabase/qiendatabase/domain/DeclaratieformulierTest.java
@@ -14,7 +14,7 @@ public class DeclaratieformulierTest {
 		
 		this.declaratieformulier.setTest("WelDus");
 		
-		Assert.assertEquals("WelDus", this.declaratieformulier.getTest());
+		Assert.assertEquals("NietDus", this.declaratieformulier.getTest());
 		
 	}
 
