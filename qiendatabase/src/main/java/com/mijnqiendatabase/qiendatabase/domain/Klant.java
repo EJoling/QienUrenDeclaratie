074e766572c327b9a236fad0e0db9dac5f0e4c2b
EJoling <emieljoling@gmail.com>
2019-04-12 14:03:30 +0200

    emailadres en getter en setter aan user toegevoegd

--- qiendatabase/src/main/java/com/mijnqiendatabase/qiendatabase/domain/Klant.java
+++ qiendatabase/src/main/java/com/mijnqiendatabase/qiendatabase/domain/Klant.java
@@ -6,10 +6,23 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 public class Klant extends User{
+	@JsonIgnoreProperties("klant")
 	@OneToMany (fetch = FetchType.EAGER)
 	private Set<Trainee> trainee;// Een klant heeft een of meer trainees die bij hem werkt
+
+	private String bedrijf;
+	
+	public String getBedrijf() {
+		return bedrijf;
+	}
+
+	public void setBedrijf(String bedrijf) {
+		this.bedrijf = bedrijf;
+	}
 
 	public void goedkeuren() {
 
